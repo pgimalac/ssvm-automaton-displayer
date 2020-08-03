6e7e1204e2e0883689f1387f8954d25080d30e2f
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-08-03 23:32:46 +0200

    update readme, add command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use crate::lib::regex_to_svg;
+use std::io::BufRead;
 
 mod lib;
 
 fn main() {
-    println!("{:?}", regex_to_svg("a.+b*+cd?", 1, 1));
+    println!(
+        "{}",
+        regex_to_svg(
+            &std::env::args()
+                .nth(1)
+                .or_else(|| std::io::stdin()
+                    .lock()
+                    .lines()
+                    .next()
+                    .map(Result::ok)
+                    .flatten())
+                .expect("A regex must be provided on stdin"),
+            1,
+            1
+        )
+    );
 }
